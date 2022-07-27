f941e8d68c69191df9032ea1213dccdd12f079bc
CNLPY <calpay@yahoo.com>
2022-07-27 09:33:21 +0300

    Initial Commit

---
+++ BasicSave/BasicSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSaveApp.swift
+//  BasicSave
+//
+//  Created by Can on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BasicSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
